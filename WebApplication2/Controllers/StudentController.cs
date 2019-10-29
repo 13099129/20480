73b3ed1f273fbceea57be0d630d55c25c546b1bb
13099129 <51092010+13099129@users.noreply.github.com>
2019-10-29 12:13:25 -0700

    Add files via upload

---
+++ WebApplication2/Controllers/StudentController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication2.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        public  IActionResult Save()
+        {
+            //    Console.WriteLine(this.Request.Form["firstname"]);
+            //    Console.WriteLine(this.Request.Form["lastname"]);
+
+
+            return View();
+        }
+    }
+}
