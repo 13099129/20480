4e0d3e0f4cce3153823d0a42e0a8d9f8258fd1bf
13099129 <51092010+13099129@users.noreply.github.com>
2019-11-04 09:00:04 -0500

    Add files via upload

---
+++ mod3/JavascriptDemo/table/TableScript.js
@@ -0,0 +1,16 @@
+﻿
+
+
+function SetUp() {
+    var rows = document.querySelectorAll("table tr");
+    //alert(rows.length);
+    for (let i = 0; i < rows.length; i++) {
+        rows[i].setAttribute("draggable", "true");
+
+        rows[i].addEventListener("drag", function (ev) {
+            alert("drag");
+            ev.dataTransfer.effectAllowed = "copy";
+            ev.dataTransfer.setData("text/plain", ev.target.outerHTML);
+        });
+    }
+}
